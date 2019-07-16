da5a5000ba318218a06842c676b6c3b4f6e557c9
dai <chamestica@gmail.com>
2019-07-16 17:51:10 +0900

    Rewrite test from unit test to integration test

--- test/index.test.js
+++ test/index.test.js
@@ -1,24 +1,12 @@
 const path = require('path')
 const fs = require('fs-extra')
-const { execSync } = require('child_process')
+const CompileEjsTask = require('../src/compile-ejs-task')
 
-const rootDirAbsolute = path.resolve(__dirname, '..')
-// Command of Laravel Mix
-const commandProd = `NODE_ENV=production ${rootDirAbsolute}/node_modules/webpack/bin/webpack.js --progress --hide-modules --config=${rootDirAbsolute}/node_modules/laravel-mix/setup/webpack.config.js --env.mixfile=tmp/webpack.mix`
-
-test('`npm run prod` outputs correctly', () => {
-  fs.copySync(
-    path.resolve(__dirname, './fixtures/webpack.mix.js'),
-    path.resolve(__dirname, '../tmp/webpack.mix.js')
-  )
-  fs.copySync(
-    path.resolve(__dirname, './fixtures/index.ejs'),
-    path.resolve(__dirname, '../tmp/resources/index.ejs')
-  )
+test('CompileEjsTask.compile(fromFileRelative, toDirRelative)', async () => {
   let result = false
   try {
-    execSync(commandProd)
-    const outputBuf = fs.readFileSync(path.resolve(__dirname, '../tmp/public/index.html'))
+    await CompileEjsTask.compile('test/fixtures/index.ejs', 'tmp')
+    const outputBuf = fs.readFileSync(path.resolve(__dirname, '../tmp/index.html'))
     const correctBuf = fs.readFileSync(path.resolve(__dirname, './fixtures/index.html'))
     result = outputBuf.equals(correctBuf)
   } catch (err) {
