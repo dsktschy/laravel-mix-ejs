93d2fc3b51986ca9c5af8334f7997a00938b11d8
dai <chamestica@gmail.com>
2019-07-20 22:44:16 +0900

    Test to run correctly for multiple files

--- __tests__/compile-ejs-task.js
+++ __tests__/compile-ejs-task.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const fs = require('fs-extra')
+const globby = require('globby')
 const CompileEjsTask = require('../src/compile-ejs-task')
 
 test('CompileEjsTask.compile(fromFileRelative, toDirRelative)', async () => {
@@ -37,14 +38,19 @@ test('CompileEjsTask.remove(fromFileRelative, toDirRelative)', () => {
 test('compileEjsTask.run()', async () => {
   let result = false
   try {
-    const originalFileAbsolute = path.resolve(__dirname, './fixtures/resources/index.ejs')
-    const targetFileAbsolute = path.resolve(__dirname, '../tmp/resources/index.ejs')
-    fs.copySync(originalFileAbsolute, targetFileAbsolute)
+    const originalDirAbsolute = path.resolve(__dirname, './fixtures/resources')
+    const targetDirAbsolute = path.resolve(__dirname, '../tmp/resources')
+    fs.copySync(originalDirAbsolute, targetDirAbsolute)
     await new CompileEjsTask({ from: 'tmp/resources/**/*.ejs', to: 'tmp/public' })
       .run()
-    const outputBuf = fs.readFileSync(path.resolve(__dirname, '../tmp/public/index.html'))
-    const correctBuf = fs.readFileSync(path.resolve(__dirname, './fixtures/public/index.html'))
-    result = outputBuf.equals(correctBuf)
+    const options = { onlyFiles: true }
+    const createBuf = fileRelative =>
+      fs.readFileSync(path.resolve(__dirname, '../', fileRelative))
+    const outputBufList = globby.sync('tmp/public', options).map(createBuf)
+    const correctBufList = globby.sync('__tests__/fixtures/public', options).map(createBuf)
+    const equalsToCorrectBuf =
+      (outputBuf, i) => outputBuf.equals(correctBufList[i])
+    result = outputBufList.every(equalsToCorrectBuf)
   } catch (err) {
     console.error(err)
   }
